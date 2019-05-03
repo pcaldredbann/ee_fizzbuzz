61951a5f7febc01b55fb2d5c4bf036ee725dbeb2
Paul Aldred-Bann <Paul.Aldred-Bann@positive-solutions.co.uk>
2019-05-03 23:42:09 +0100

    completed code for step three, corrected unit tests to match spec with 100pct coverage report

--- src/index.js
+++ src/index.js
@@ -9,24 +9,43 @@ const runFizzBuzzer = function (start, end) {
 
   if (start > end) throw 'Starting value cannot be greater than end value.';
 
+  const report = {
+    fizzCount: 0,
+    buzzCount: 0,
+    fizzBuzzCount: 0,
+    luckyCount: 0,
+    integerCount: 0
+  }
+
   const result = [];
   for (let cursor = start; cursor <= end; cursor++) {
     if (indexOf(cursor.toString(), '3') > -1) {
+      report.luckyCount++;
       result.push('lucky');
     }
     else if (cursor % 15 === 0) {
+      report.fizzBuzzCount++;
       result.push('fizzbuzz');
     }
     else if (cursor % 3 === 0) {
+      report.fizzCount++;
       result.push('fizz');
     }
     else if (cursor % 5 === 0) {
+      report.buzzCount++;
       result.push('buzz');
     }
     else {
+      report.integerCount++;
       result.push(cursor.toString());
     }
   }
+
+  result.push('fizz: ' + report.fizzCount.toString());
+  result.push('buzz: ' + report.buzzCount.toString());
+  result.push('fizzbuzz: ' + report.fizzBuzzCount.toString());
+  result.push('lucky: ' + report.luckyCount.toString());
+  result.push('integer: ' + report.integerCount.toString());
 
   return join(result, ' ');
 }
--- src/index.test.js
+++ src/index.test.js
@@ -7,37 +7,37 @@ describe('Equal Experts FizzBuzzer', () => {
   it('it returns the correct result for a single integer', () => {
     const series = runFizzBuzzer(1, 1);
 
-    expect(series).toEqual('1');
+    expect(series).toEqual('1 fizz: 0 buzz: 0 fizzbuzz: 0 lucky: 0 integer: 1');
   });
 
   it('it returns the correct result for a single value 3', () => {
     const series = runFizzBuzzer(3, 3);
 
-    expect(series).toEqual('lucky');
+    expect(series).toEqual('lucky fizz: 0 buzz: 0 fizzbuzz: 0 lucky: 1 integer: 0');
   });
 
   it('it returns the correct result for a single value 13', () => {
     const series = runFizzBuzzer(13, 13);
 
-    expect(series).toEqual('lucky');
+    expect(series).toEqual('lucky fizz: 0 buzz: 0 fizzbuzz: 0 lucky: 1 integer: 0');
   });
 
   it('it returns the correct result for a single value 5', () => {
     const series = runFizzBuzzer(5, 5);
 
-    expect(series).toEqual('buzz');
+    expect(series).toEqual('buzz fizz: 0 buzz: 1 fizzbuzz: 0 lucky: 0 integer: 0');
   });
 
   it('it returns the correct result for a single value 15', () => {
     const series = runFizzBuzzer(15, 15);
 
-    expect(series).toEqual('fizzbuzz');
+    expect(series).toEqual('fizzbuzz fizz: 0 buzz: 0 fizzbuzz: 1 lucky: 0 integer: 0');
   });
 
   it('it returns the correct result for a series of numbers between 1 and 20', () => {
     const series = runFizzBuzzer(1, 20);
     
-    expect(series).toEqual('1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz');
+    expect(series).toEqual('1 2 lucky 4 buzz fizz 7 8 fizz buzz 11 fizz lucky 14 fizzbuzz 16 17 fizz 19 buzz fizz: 4 buzz: 3 fizzbuzz: 1 lucky: 2 integer: 10');
   });
 
   it('it throws the expected exception if start value is missing or invalid', () => {
